7e0d12f22dfb6405ebc263aa59f19d9ee91748b6
Marcelo Sica <marcelosica10@gmail.com>
2023-04-26 04:00:27 +0000

    Proyecto Data Modeling Star Wars

--- src/models.py
+++ src/models.py
@@ -9,40 +9,40 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'User'
-    ID = Column(Integer, primary_key=True)
-    Username = Column(String(250), nullable=False)
-    FirstName = Column(String(250), nullable=False)
-    LastName = Column(String(250), nullable=False)
-    Email = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     pass
 
 class Post(Base):
     __tablename__ = 'Post'
-    ID = Column(Integer, primary_key=True)
-    UserID = Column(Integer, ForeignKey('User.id'))
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
     pass
 
 class Comment(Base):
     __tablename__ = 'Comment'
-    ID = Column(Integer, primary_key=True)
-    CommentText = Column(String(250), nullable=False)
-    AuthorID = Column(Integer, ForeignKey('User.id'))
-    PostID = Column(Integer, ForeignKey('Post.id'))
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('User.id'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
     pass
 
 class Media(Base):
     __tablename__ = 'Media'
-    ID = Column(Integer, primary_key=True)
-    Type = Column(Enum, nullable=False)
-    URL = Column(String(250), nullable=False)
-    PostID = Column(Integer, ForeignKey('Post.id'))
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum, nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'))
     pass
 
 class Follower(Base):
     __tablename__ = 'Follower'
-    ID = Column(Integer, primary_key=True)
-    UserFromID = Column(Integer, ForeignKey('User.id'))
-    UserToID = Column(Integer, ForeignKey('User.id'))
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('User.id'))
+    user_to_id = Column(Integer, ForeignKey('User.id'))
     pass
 
 ## Draw from SQLAlchemy base
